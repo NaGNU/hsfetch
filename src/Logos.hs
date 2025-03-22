925f89c97caa98d169f2d8ac3401a70dcf522894
Deny ruzen <123930771+ruzen42@users.noreply.github.com>
2025-03-22 12:07:24 +0500

    Update Logos.hs

--- src/Logos.hs
+++ src/Logos.hs
@@ -1,1 +1,150 @@
+module Rfetch.Logos 
+  ( archLogo
+  , debianLogo
+  , slackwareLogo
+  , fedoraLogo
+  , gentooLogo
+  , voidLogo
+  , alpineLogo
+  , ubuntuLogo
+  , opensuseLogo
+  , artixLogo
+  , kaliLogo
+  , nixosLogo
+  , parrotLogo
+  , endeavourLogo
+  , mintLogo
+  ) where
+
+archLogo :: String
+archLogo = unlines
+  [ "      /\\  "
+  , "     /  \\" 
+  , "    / /\\ \\" 
+  , "   / ____ \\" 
+  , "  /_/    \\_\\" 
+  ]
+
+debianLogo :: String
+debianLogo = unlines
+  [ "      _____ "
+  , "     /  __ \\\"
+  , "    |  /    \\\"
+  , "     \\_\\____/ "
+  , "        \\_/    "
+  ]
+
+slackwareLogo :: String
+slackwareLogo = unlines
+  [ "    ________ "
+  , "   / ____  /"
+  , "  / / __/ / "
+  , " / /_/ /_/  "
+  , "/________/  "
+  ]
+
+fedoraLogo :: String
+fedoraLogo = unlines
+  [ "    _______ "
+  , "   /  ___/ "
+  , "  /  /__   "
+  , " /_____/   "
+  ]
+
+gentooLogo :: String
+gentooLogo = unlines
+  [ "   ___     "
+  , "  / __\\  "
+  , " / /  \\" 
+  , " \\_\\__/  "
+  ]
+
+voidLogo :: String
+voidLogo = unlines
+  [ "   __     "
+  , "  /  \\" 
+  , " |  ()  |"
+  , "  \\__/ "
+  ]
+
+alpineLogo :: String
+alpineLogo = unlines
+  [ "     /\\   "
+  , "    /  \\" 
+  , "   / /\\ \\" 
+  , "  / /__\\ \\" 
+  , " /_/    \\_\\" 
+  ]
+
+ubuntuLogo :: String
+ubuntuLogo = unlines
+  [ "   .--.   "
+  , "  |o_o |  "
+  , "  |:_/ |  "
+  , " //   \\" 
+  , "( |   | ) "
+  , " \\_\\_//_  "
+  ]
+
+opensuseLogo :: String
+opensuseLogo = unlines
+  [ "    .----.   "
+  , "   / .==.\\  "
+  , "  / /    \\" 
+  , "  \\ \\____// "
+  , "   \\_____/  "
+  ]
+
+artixLogo :: String
+artixLogo = unlines
+  [ "     /\\   "
+  , "    /  \\" 
+  , "   / /\\ \\" 
+  , "  / ____ \\" 
+  , " /_/    \\_\\" 
+  ]
+
+kaliLogo :: String
+kaliLogo = unlines
+  [ "     (\\  "
+  , "      )) "
+  , "     //  "
+  , "   ==\\=  "
+  ]
+
+nixosLogo :: String
+nixosLogo = unlines
+  [ "   \\ \\   / / "
+  , "    \\ \\_/ /  "
+  , "     \\___/   "
+  , "    / ___ \\" 
+  , "   / /   \\" 
+  , "  /_/     \\" 
+  ]
+
+parrotLogo :: String
+parrotLogo = unlines
+  [ "   ( \\" 
+  , "    ) ) "
+  , "   ( (  "
+  , "    )_) "
+  ]
+
+endeavourLogo :: String
+endeavourLogo = unlines
+  [ "     /\\   "
+  , "    /  \\" 
+  , "   / /\\ \\" 
+  , "  / ____ \\" 
+  , " /_/    \\_\\" 
+  ]
+
+mintLogo :: String
+mintLogo = unlines
+  [ "   _____   "
+  , "  |  __ \\" 
+  , "  | |__) | "
+  , "  |  ___/  "
+  , "  |_|      "
+  ]
 
